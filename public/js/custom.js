df7e80e4b5e912569841a50d0b5a261ab96f2714
Nguyễn Tuấn <Nguyn Tun>
2016-05-11 12:02:35 +0700

    Final 060

--- public/js/custom.js
+++ public/js/custom.js
@@ -2,6 +2,29 @@ $(function(){
 
 Stripe.setPublishableKey('pk_test_xY5ZPRsgSOl2KrFag0TiQQCG');
 
+var opts = {
+  lines: 13 // The number of lines to draw
+  , length: 28 // The length of each line
+  , width: 33 // The line thickness
+  , radius: 6 // The radius of the inner circle
+  , scale: 1.5 // Scales overall size of the spinner
+  , corners: 0.9 // Corner roundness (0..1)
+  , color: '#000' // #rgb or #rrggbb or array of colors
+  , opacity: 0.2 // Opacity of the lines
+  , rotate: 0 // The rotation offset
+  , direction: 1 // 1: clockwise, -1: counterclockwise
+  , speed: 1 // Rounds per second
+  , trail: 70 // Afterglow percentage
+  , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+  , zIndex: 2e9 // The z-index (defaults to 2000000000)
+  , className: 'spinner' // The CSS class to assign to the spinner
+  , top: '50%' // Top position relative to parent
+  , left: '50%' // Left position relative to parent
+  , shadow: false // Whether to render a shadow
+  , hwaccel: false // Whether to use hardware acceleration
+  , position: 'absolute' // Element positioning
+}
+
   $('#search').keyup(function(){
     var search_term=$(this).val();
     $.ajax({
@@ -35,7 +58,7 @@ Stripe.setPublishableKey('pk_test_xY5ZPRsgSOl2KrFag0TiQQCG');
       }
     });
 });
-});
+
 
 $(document).on('click','#plus',function(e){
   e.preventDefault();
@@ -83,6 +106,9 @@ function stripeResponseHandler(status, response) {
     // Insert the token ID into the form so it gets submitted to the server:
     $form.append($('<input type="hidden" name="stripeToken">').val(token));
 
+    var spinner = new Spinner(opts).spin();
+    $('#loading').append(spinner.el);
+
     // Submit the form:
     $form.get(0).submit();
   }
@@ -99,3 +125,4 @@ var $form = $('#payment-form');
     // Prevent the form from being submitted:
     return false;
   });
+});
